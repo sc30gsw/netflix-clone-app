4e9bd5e473a75ad6cdaee517aa3673355df010d3
abi-kaki <kaito.kaki@abi-inc.co.jp>
2023-03-28 14:43:41 +0900

    APIからデータを取得し一覧で表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Row } from "./components/Row";
+import { requests } from "./request";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.feactDocumentMovies} />
+    </div>
+  );
 };
 
 export default App;
--- src/axios.ts
+++ src/axios.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 //TMDBからのbaseURLリクエストを作成
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: process.env.REACT_APP_API_KEY,
+  },
 });
 
 export default instance;
